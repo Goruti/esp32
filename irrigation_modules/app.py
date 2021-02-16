82c5c9c1d8456a6956f51c8dd9078d13035f171d
Diego Antonino <dantonino@.mnubo.com>
2021-02-15 22:49:09 -0500

    increase report tim to 10min

--- irrigation_modules/app.py
+++ irrigation_modules/app.py
@@ -15,7 +15,7 @@ def main_app():
     try:
         loop = asyncio.get_event_loop()
         loop.create_task(main_loops.initialize_rtc(frequency_loop=3600))
-        loop.create_task(main_loops.reading_moister(frequency_loop_ms=600000, report_freq_ms=3600000))
+        loop.create_task(main_loops.reading_moister(frequency_loop_ms=600000, report_freq_ms=600000))
         loop.create_task(main_loops.reading_water_level())
         webServer.webapp.run(host="0.0.0.0", port=80)
 
